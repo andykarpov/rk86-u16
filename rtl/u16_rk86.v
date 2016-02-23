7ee6e02d998b1f43899e83d09734b7d14b340d6a
Andy Karpov <andy.karpov@gmail.com>
2016-02-23 14:44:52 +0200

    some refactoring

--- rtl/u16_rk86.v
+++ rtl/u16_rk86.v
@@ -78,7 +78,7 @@ wire reset = ~reset_n;
 reg[3:0] KEY = {4'b1111}; // missed hw buttons on reverse-u16, all keys are pulled up
 
 always @(posedge clk50mhz) begin
-	if (KEY[0] && k_reset==1'b0 && reset_cnt==4'd14)
+	if (KEY[0] && k_reset==1'b0 && clock_locked==1'b0 && reset_cnt==4'd14)
 		reset_n <= 1'b1;
 	else begin
 		reset_n <= 1'b0;
